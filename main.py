26b0daacf3034858779be60954ae38fe0dbf5cb4
zinhour10 <zinhour10@gmail.com>
2025-08-19 10:00:47 +0700

    p

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ["PORT"])
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    
